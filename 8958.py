98c9c78954c7a09e4de24c883b57990e0f989930
monlaoi002 <qkrwjdgns270@gmail.com>
2025-03-14 15:29:14 +0900

    Update 8958.py

--- 8958.py
+++ 8958.py
@@ -1,1 +1,15 @@
-c
+a = int(input())
+for _ in range(a):
+    scorrr = 0
+    gajung = 0
+    s = input()
+    
+    for i in s:
+        if i == "O":
+            gajung += 1
+            scorrr += gajung
+        else:
+            gajung = 0
+            
+    print(scorrr)
+    
